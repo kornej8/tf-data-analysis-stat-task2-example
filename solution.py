989eccff268aad8a41895f09e237bde5ed2d6867
Kornej Shchepalov <115864132+kornej8@users.noreply.github.com>
2023-04-06 03:02:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ chat_id = 720721680 # Ваш chat ID, не меняйте название пе�
 
 def solution(p: float, x: np.array) -> float:
     alpha = 1 - p
-    loc = sum(list(map(lambda x: x**2, a)))
+    loc = sum(list(map(lambda x: x**2, x)))
     left = np.sqrt(loc / 40 * chi2.ppf(alpha / 2, df = 2*len(x)) / np.sqrt(len(x)))
     right = np.sqrt(loc / 40 * chi2.ppf(1 - alpha / 2, df = 2*len(x)) / np.sqrt(len(x)))
     return left, \
