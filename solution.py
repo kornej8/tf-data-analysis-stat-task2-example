559df7fbc684d3c4f4c5b4f3a7ab8ab26967e9f5
Kornej Shchepalov <115864132+kornej8@users.noreply.github.com>
2023-04-08 00:58:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,12 +7,15 @@ from scipy.stats import norm, chi2
 chat_id = 720721680 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> float:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
     alpha = 1 - p
     n = len(x)
-    s = sum([(x_i - x.mean())**2 for x_i in list(x)]) / n
+    s = sum(list(map(lambda i: i**2, x)))
     left_chi = chi2.ppf(1 - alpha / 2, df = 2*len(x)-2)
     rigth_chi = chi2.ppf(alpha / 2, df = 2*len(x)-2)
-    left = np.sqrt(((n-1) * s**2) / (40 * left_chi))
-    right = np.sqrt(((n-1) * s**2) / (40 * rigth_chi))
+    left = np.sqrt( s / (40 * left_chi))
+    right = np.sqrt( s / (40 * rigth_chi))
     return left, \
            right
